b7e124002152483d01ccaf2b2f55e11b9523aeb4
andrMollo <119691950+andrMollo@users.noreply.github.com>
2023-09-17 13:29:07 +0200

    Test command
    - Add xCAD.NET reference
    - Add test command

---
+++ SolidWorksAutomator/SolidWorksAutomatorAddIn.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xarial.XCad.UI.Commands;
+
+namespace SolidWorksAutomator
+{
+    [System.Runtime.InteropServices.ComVisible(true)]
+    public class SolidWorksAutomatorAddIn : Xarial.XCad.SolidWorks.SwAddInEx
+    {
+        public enum SWACommands_e
+        {
+            TestComand
+        }
+        
+        public override void OnConnect()
+        {
+            CommandManager.AddCommandGroup<SWACommands_e>().CommandClick += OnCommandClick;
+        }
+
+        private void OnCommandClick(SWACommands_e spec)
+        {
+            switch (spec)
+            {
+                case SWACommands_e.TestComand:
+                    Application.ShowMessageBox("Test command");
+                    break;
+            }
+        }
+    }
+}
