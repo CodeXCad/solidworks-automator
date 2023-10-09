7eaa0b750c02696542cc31613b1fac6d729d25ea
andrMollo <119691950+andrMollo@users.noreply.github.com>
2023-10-09 10:10:16 +0200

    Add open file control

--- SolidWorksAutomator/MacrosProperty/SetAuthorLogic.cs
+++ SolidWorksAutomator/MacrosProperty/SetAuthorLogic.cs
@@ -1,8 +1,11 @@
-﻿using System;
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 
 namespace SolidWorksAutomator
 {
@@ -13,6 +16,17 @@ namespace SolidWorksAutomator
         /// </summary>
         public static void SetAuthor()
         {
+            // Check if there is an open file
+            ModelDoc2 swModel = SolidWorksAutomatorAddIn.swApp.IActiveDoc2;
+
+            if (swModel is null)
+            {
+                MessageBox.Show("Open a document to set the author.");
+            }
+            // Check if something is selected
+            // If something is selected get the list of model objects
+            // If nothing is selected add the active model to the list of model object
+            // SetCustomProperty to the list of model object
             var prpManager = new SAPropertyManager();
 
             string PRP_NAME = "Disegnatore";
