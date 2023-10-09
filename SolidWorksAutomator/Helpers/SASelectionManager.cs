4dfc4ffa9b7c6ccadfdba44a9747961f2757522c
Andrea Mollo <andr.mollo@gmail.com>
2023-10-09 14:12:49 +0200

    Create SASelectionManager.cs

---
+++ SolidWorksAutomator/Helpers/SASelectionManager.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorksAutomator.Helpers
+{
+    internal class SASelectionManager
+    {
+        /// <summary>
+        /// Get the selected components
+        /// </summary>
+        public void GetSelectedComponent()
+        {
+
+        }
+    }
+}
