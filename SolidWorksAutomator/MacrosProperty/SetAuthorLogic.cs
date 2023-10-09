67872a296eeb939c813cb8cecb5071aac620b31e
Andrea Mollo <andr.mollo@gmail.com>
2023-10-09 12:54:38 +0200

    Add file type validation

--- SolidWorksAutomator/MacrosProperty/SetAuthorLogic.cs
+++ SolidWorksAutomator/MacrosProperty/SetAuthorLogic.cs
@@ -5,7 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Windows;
+using System.Windows.Forms;
 
 namespace SolidWorksAutomator
 {
@@ -16,13 +16,32 @@ namespace SolidWorksAutomator
         /// </summary>
         public static void SetAuthor()
         {
-            // Check if there is an open file
             ModelDoc2 swModel = SolidWorksAutomatorAddIn.swApp.IActiveDoc2;
 
+            #region Pre-conditions validation
+            // Check if there is an open file
             if (swModel is null)
             {
-                MessageBox.Show("Open a document to set the author.");
+                MessageBox.Show("Open a document to set the author.",
+                    caption:"Set file author",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                // TODO ? Need to be move to a method?
+                return;
             }
+
+            // Check that open file isn't a drawing
+            if (swModel.GetType() == (int)swDocumentTypes_e.swDocDRAWING)
+            {
+                MessageBox.Show("Open a part or an assembly file to set the author.",
+                    caption: "Set file author",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            #endregion
+
             // Check if something is selected
             // If something is selected get the list of model objects
             // If nothing is selected add the active model to the list of model object
