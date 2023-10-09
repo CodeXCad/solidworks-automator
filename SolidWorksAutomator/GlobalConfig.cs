be2b8a49c370d5d945cc72ca58e426d82993a1d9
andrMollo <119691950+andrMollo@users.noreply.github.com>
2023-10-09 17:53:42 +0200

    Add Vault name to GlobalConfig

---
+++ SolidWorksAutomator/GlobalConfig.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorksAutomator
+{
+    public static class GlobalConfig
+    {
+        public const string VaultName = "VAULT_BL";
+    }
+}
