fab5adc3ea8707b04b70eab5cc01653dd0c4e81c
Andrea Mollo <andr.mollo@gmail.com>
2023-10-09 13:44:59 +0200

    Squashed commit of the following:
    commit 67872a296eeb939c813cb8cecb5071aac620b31e
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Mon Oct 9 12:54:38 2023 +0200
    
        Add file type validation
    
    commit 7eaa0b750c02696542cc31613b1fac6d729d25ea
    Author: andrMollo <119691950+andrMollo@users.noreply.github.com>
    Date:   Mon Oct 9 10:10:16 2023 +0200
    
        Add open file control
    
    commit 6a15d76395530c2c8db17bb9124163652c35008d
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Mon Oct 9 08:09:26 2023 +0200
    
        Add summary to SetAuthor method
    
    commit db5f3d2bc4bbe2f584fb4c08f4c0f4961e213800
    Author: andrMollo <119691950+andrMollo@users.noreply.github.com>
    Date:   Mon Oct 9 07:58:21 2023 +0200
    
        Rename SetAuthor class
    
    commit 9d40b807a9b310a832bf8b439025066a096fd688
    Author: andrMollo <119691950+andrMollo@users.noreply.github.com>
    Date:   Mon Oct 9 07:57:42 2023 +0200
    
        Move SetAuthor to its own class
    
    commit 5f84d75b874645cafdbd3d16cc472faef3bf9c7a
    Author: andrMollo <119691950+andrMollo@users.noreply.github.com>
    Date:   Fri Oct 6 17:09:24 2023 +0200
    
        Re-organize commands in different methods
    
    commit 5561624c6ae24364547206e1b5b1eda8be7d87cc
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Fri Oct 6 17:00:37 2023 +0200
    
        Update xml comments
    
    commit d2bd1c1158a5745ee0803386b8439dfcd74acf23
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Fri Oct 6 17:00:01 2023 +0200
    
        New method delete author property
    
    commit db826acba2b63ce92074380646560554485af2a7
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Fri Oct 6 16:47:03 2023 +0200
    
        Define swApp as public
    
    commit 76d17b2eb121e3ebf0868cfa4ee46279132dfadd
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Fri Oct 6 15:59:40 2023 +0200
    
        Update comments
    
    commit 7c8c99503d0a359eb254077877419c728d5981d5
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Fri Oct 6 15:51:29 2023 +0200
    
        Pass string parameters to SetCustomProperty method
    
    commit d5e518b8344bbfd4fc91f41121aca6fc8abb1eae
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Fri Oct 6 15:45:11 2023 +0200
    
        Write string to custom property
    
    commit 7b4b86dedfe50438194cfd7ec53acfc867e3bd53
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Tue Sep 19 18:25:55 2023 +0200
    
        Set author first draft
    
    commit 70a7164e2e097a6f8d7771f4d0f7d59cccd8dcbc
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Tue Sep 19 14:24:46 2023 +0200
    
        Create method to open document
    
    commit 844b01cf1b109e6e8aa328178a9efdae48368aca
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Tue Sep 19 13:57:11 2023 +0200
    
        Define public variable for SW application
    
    commit 41861b2738aee19331143d65d9e881510bf684cf
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Mon Sep 18 17:57:09 2023 +0200
    
        Send message via application
    
    commit 3de654d77ec33306cbcdae19a0bbde0a358dc986
    Author: Andrea Mollo <andr.mollo@gmail.com>
    Date:   Mon Sep 18 17:47:44 2023 +0200
    
        Test get swApplication

---
+++ SolidWorksAutomator/MacrosProperty/SetAuthorLogic.cs
@@ -0,0 +1,57 @@
+﻿using SolidWorks.Interop.sldworks;
+using SolidWorks.Interop.swconst;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SolidWorksAutomator
+{
+    public class SetAuthorLogic
+    {
+        /// <summary>
+        /// Write the author in the custom properties
+        /// </summary>
+        public static void SetAuthor()
+        {
+            ModelDoc2 swModel = SolidWorksAutomatorAddIn.swApp.IActiveDoc2;
+
+            #region Pre-conditions validation
+            // Check if there is an open file
+            if (swModel is null)
+            {
+                MessageBox.Show("Open a document to set the author.",
+                    caption:"Set file author",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                // TODO ? Need to be move to a method?
+                return;
+            }
+
+            // Check that open file isn't a drawing
+            if (swModel.GetType() == (int)swDocumentTypes_e.swDocDRAWING)
+            {
+                MessageBox.Show("Open a part or an assembly file to set the author.",
+                    caption: "Set file author",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            #endregion
+
+            // Check if something is selected
+            // If something is selected get the list of model objects
+            // If nothing is selected add the active model to the list of model object
+            // SetCustomProperty to the list of model object
+            var prpManager = new SAPropertyManager();
+
+            string PRP_NAME = "Disegnatore";
+            string PRP_VALUE = "Mollo A.";
+
+            prpManager.SetCustomProperty(PRP_NAME, PRP_VALUE);
+        }
+    }
+}
