045435f469f291e33c77852b041d9b419d5934fd
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-10-11 16:53:15 +0200

    Add ExportFile class

---
+++ SolidWorksAutomator/MacrosFile/ExportFileMacro.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SolidWorksAutomator
+{
+    internal class ExportFileMacro
+    {
+        public static void ExportFile()
+        {
+
+        }
+    }
+}
--- SolidWorksAutomator/SolidWorksAutomatorAddIn.cs
+++ SolidWorksAutomator/SolidWorksAutomatorAddIn.cs
@@ -15,15 +15,17 @@ namespace SolidWorksAutomator
     public class SolidWorksAutomatorAddIn : Xarial.XCad.SolidWorks.SwAddInEx
     {
         [Title("SolidWorks Automator")]
+        [Description("A collection of SolidWorks macro")]
         public enum SWACommands_e
         {
             [Title("Set author")]
             [Description("Write the author in the custom properties")]
             SetAuthor,
-            [Title("Delete author")]
-            [Description("Delete the author from the custom property")]
-            DeleteAuthor
+            [Title("Export file")]
+            [Description("Export the open file in multiple formats")]
+            ExportFile
         }
+
         public static SldWorks swApp { get; set; }
 
         public override void OnConnect()
@@ -41,19 +43,10 @@ namespace SolidWorksAutomator
                 case SWACommands_e.SetAuthor:
                     SetAuthorMacro.SetAuthor();
                     break;
-                case SWACommands_e.DeleteAuthor:
-                    DeleteAuthor();
+                case SWACommands_e.ExportFile:
+                    ExportFileMacro.ExportFile();
                     break;
             }
-        }
-
-        private void DeleteAuthor()
-        {
-            var prpMgr = new SAPropertyManager();
-
-            string prpName = "Disegnatore";
-
-            prpMgr.SetCustomPropertyEmpty(prpName);
         }
     }
 }
