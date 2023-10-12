abb7400bc1227093dbe6a586141b21837998e9cf
Andrea Mollo <119691950+andrMollo@users.noreply.github.com>
2023-10-12 17:44:28 +0200

    Update comment format

--- SolidWorksAutomator/MacrosFile/ExportFileMacro.cs
+++ SolidWorksAutomator/MacrosFile/ExportFileMacro.cs
@@ -13,27 +13,29 @@ namespace SolidWorksAutomator
         /// </summary>
         public static void ExportFile()
         {
-            // TODOs:
-            // Check if there is an open file
-            // Check if the file is saved
-            // Check what type of file is open
-            // Process drawing
-            //  Export drawing
-            //      Update sheet format for all sheet
-            //      Export to PDF
-            //      Export to DWG
-            //  Get root model
-            //  Export model
-            // Process model
-            //  Export model
-            //      Export model to STEP
-            //      Export model to PNG
-            //  Check if drawing exists
-            //  Open drawing
-            //  Process drawing
-            // Export
-            //  Compose outpur filename
-            //  Create directory is not present
+            /*
+             TODOs:                         
+             Check if there is an open file 
+             Check if the file is saved     
+             Check what type of file is open
+             Process drawing
+              Export drawing
+                  Update sheet format for all sheet
+                  Export to PDF
+                  Export to DWG
+              Get root model
+              Export model
+             Process model
+              Export model
+                  Export model to STEP
+                  Export model to PNG
+              Check if drawing exists
+              Open drawing
+              Process drawing
+             Export
+              Compose output filename
+              Create directory is not present
+            */
         }
     }
 }
